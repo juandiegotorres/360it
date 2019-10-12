042e6149f3f70873db3be3c1655aa20b82d06963
juandiegotorres <56370225+juandiegotorres@users.noreply.github.com>
2019-10-12 11:43:53 -0300

    productos (clase)

---
+++ Entidades/Producto.vb
@@ -0,0 +1,116 @@
+﻿Public Class Producto
+    Dim capaDatos As New CapaDeNegocios.cdDatosPrueba
+#Region "Declaración de propiedes"
+
+    Private _idProducto As UInteger
+    Public Property idProducto As UInteger
+        Set(value As UInteger)
+            _idProducto = value
+        End Set
+        Get
+            Return _idProducto
+        End Get
+    End Property
+
+    Private _nombProducto As String
+    Public Property nombProducto As String
+        Set(value As String)
+            _nombProducto = value
+        End Set
+        Get
+            Return _nombProducto
+        End Get
+    End Property
+
+    Private _modelo As String
+    Public Property modelo As String
+        Set(value As String)
+            _modelo = value
+        End Set
+        Get
+            Return _modelo
+        End Get
+    End Property
+
+
+    Private _idProveedor As UInt16
+    Public Property idProveedor As UInt16
+        Set(value As UInt16)
+            _idProveedor = value
+        End Set
+        Get
+            Return _idProveedor
+        End Get
+    End Property
+
+    Private _nombProveedor As String
+    Public Property nombProveedor As String
+        Set(value As String)
+            _nombProveedor = value
+        End Set
+        Get
+            Return _nombProveedor
+        End Get
+    End Property
+
+    Private _categoria As UInt16
+    Public Property categoria As UInt16
+        Set(value As UInt16)
+            _categoria = value
+        End Set
+        Get
+            Return _categoria
+        End Get
+    End Property
+
+    Private _cantidad As UInt32
+    Public Property cantidad As UInt32
+        Set(value As UInt32)
+            _cantidad = value
+        End Set
+        Get
+            Return _cantidad
+        End Get
+    End Property
+
+    Private _precioCosto As Double
+    Public Property precioCosto As Double
+        Set(value As Double)
+            _precioCosto = value
+        End Set
+        Get
+            Return _precioCosto
+        End Get
+    End Property
+
+    Private _precioVenta As Double
+    Public Property precioVenta As Double
+        Set(value As Double)
+            _precioVenta = value
+        End Set
+        Get
+            Return _precioVenta
+        End Get
+    End Property
+
+    Private _descripcion As String
+    Public Property descripcion As String
+        Set(value As String)
+            _descripcion = value
+        End Set
+        Get
+            Return _descripcion
+        End Get
+    End Property
+
+#End Region
+#Region "Metodos"
+
+    Public Sub categorias(ByRef tabla As DataTable)
+        Dim consultaSQL As String = "SELECT * FROM categorias"
+        capaDatos.llenarDatos(tabla, consultaSQL)
+
+    End Sub
+
+#End Region
+End Class
--- WindowsApp1/Productos/frmNuevoProducto.vb
+++ WindowsApp1/Productos/frmNuevoProducto.vb
@@ -2,9 +2,26 @@
     Dim precioCosto As Integer
     Dim precioVenta As Integer
     Dim porcentaje As Integer
+    Dim eProducto As New Entidades.Producto
+
+
+    Private Sub frmNuevoProducto_Load(sender As Object, e As EventArgs) Handles Me.Load
+        cargarCategorias()
+    End Sub
+
     Private Sub BtnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
         Me.Close()
         frmPrincipal.Show()
+    End Sub
+
+    Public Sub cargarCategorias()
+        Dim dtCategorias As New DataTable
+        eProducto.categorias(dtCategorias)
+        With cbCategorias
+            .DataSource = dtCategorias
+            .DisplayMember = "nombCategoria"
+            .ValueMember = "idcategoria"
+        End With
     End Sub
 
     Private Sub ChbPorcentaje_CheckedChanged(sender As Object, e As EventArgs) Handles chbPorcentaje.CheckedChanged
@@ -18,6 +35,34 @@
             txtPrecioVenta.Text = ""
         End If
 
+    End Sub
+
+    Public Function comprobarDatos()
+        Dim _control As Control
+        For Each _control In Me.Controls
+            If TypeOf _control Is TextBox Then
+                If _control Is txtDescripcion Then
+                Else
+                    If _control.Text = "" Then
+                        MsgBox("Faltan completar datos", MsgBoxStyle.Exclamation, "Clientes")
+                        Return False
+
+                    End If
+                End If
+            End If
+        Next
+        Return True
+    End Function
+
+    Public Sub limpiarCampos()
+        Dim txt As Control
+        For Each txt In Me.Controls
+            If TypeOf txt Is TextBox Then
+                txt.Text = ""
+            End If
+        Next
+        cbCategorias.Text = ""
+        cbCategorias.SelectedValue = 0
     End Sub
 
     Private Sub TxtPrecioCosto_TextChanged(sender As Object, e As EventArgs) Handles txtPrecioCosto.TextChanged
@@ -39,4 +84,6 @@
             End If
         End If
     End Sub
+
+
 End Class
